bb299acbe0618ec34a68839de7fdc1f40a714b66
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-06-04 13:59:20 +0530

    Add files via upload

---
+++ sass-demo-app/src/SassApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+function SassApp()
+{
+    return (
+        <>
+            <div className='parent'>
+                <div className='box1'>Just using sass it is a prprocessor of css and provides a server side css executions used with .scss</div>
+                <div className='box2'>Welcome to Sass</div>
+            </div>
+        </>
+    )
+}
+
+export default SassApp
---
+++ sass-demo-app/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import SassApp from "./SassApp";
+import './assets/sass/style.scss'
+const root=ReactDOM.createRoot(document.getElementById("sass-demo-app"));
+root.render(
+
+    <>
+
+    <SassApp />
+
+    </>
+)
